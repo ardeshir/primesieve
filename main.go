5fcdc37289772433f8ac9d1658ce58546fb3f0aa
ardeshir sepahsalar <ardeshir.org@gmail.com>
2020-05-06 10:06:26 -0500

    typo

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
+
 import (
 	"fmt"
 )
+
 func Generate(chn chan<- int) {
-   for i := 2; i++ {
-       ch <- i                // Sedn 'i' to channel ch
-   }
+	for i := 2; ; i++ {
+		chn <- i // Sedn 'i' to channel ch
+	}
 }
 func Filter(src <-chan int, dst chan<- int, prime int) {
-    for i := range src {      // Loop over values recieved 
-       if i % prime != 0 {    
-        dst <- i              //  Send 'i' to channel 'dst'   
-       }                      
-    }                         
+	for i := range src { // Loop over values recieved
+		if i%prime != 0 {
+			dst <- i //  Send 'i' to channel 'dst'
+		}
+	}
 }
 func main() {
-    src := make(chan int)     // Create new channel.
-    go Generate(src)          // Launch Generic goroutine.
-    for i := 0; 1 < 100; i++ {// Find 100 primes
-	prime := <-src 
-	fmt.Println(prime)
-	dst := make(chan int)
-	go Filter(src, dst, prime)
-	src = dst
-    }
+	src := make(chan int)      // Create new channel.
+	go Generate(src)           // Launch Generic goroutine.
+	for i := 0; 1 < 100; i++ { // Find 100 primes
+		prime := <-src
+		fmt.Println(prime)
+		dst := make(chan int)
+		go Filter(src, dst, prime)
+		src = dst
+	}
 }
